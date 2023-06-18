8928df92a07e54b45f0e4a6235c72cfb34114ba9
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-06-17 23:49:59 -0300

    Exercício 1

---
+++ Aulas e Exercícios/aula-10/index.js
@@ -0,0 +1,11 @@
+let nome = "Henrique"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+//window.alert(nome)
+//nome = window.prompt("Insira o seu nome: ")
+let confirmação = window.confirm("Confirma essa ação?")
+
+console.log(confirmação)
