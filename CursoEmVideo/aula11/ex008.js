68e702c039ab756c447933ce37785ad9723457b7
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-07-18 00:16:22 -0300

    Condições

---
+++ CursoEmVideo/aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 60.1
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+
+if(vel > 60){
+  console.log("Você ultrapassou a velocidade permitida. MULTADO!" )
+} else {
+  console.log("Parabéns! Você está dentro da velocidade.")
+}
+
+console.log("Dirija sempre usando cinto de segurança!")
