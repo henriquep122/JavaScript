06482af27a2fc25d0aa8ed1351f7372f72cfe287
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-07-18 00:47:03 -0300

    Condição

---
+++ CursoEmVideo/aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'França'
+console.log(`Vivendo em ${pais}`)
+if(pais != 'Brasil'){ //condição composta
+  console.log('Você é Estrangeiro')
+} else {
+  console.log('Você é Brasileiro!')
+}
