c4586b7f28df9a123a393bc2529d8090ea02c910
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-07-24 19:46:42 -0300

    while

---
+++ OniBitCode/exe04/cidades.js
@@ -0,0 +1,8 @@
+var nome = prompt("Favor informar o seu nome:")
+var resposta = prompt(`Olá ${nome}, ja visitou alguma cidade? `)
+
+while (resposta == sim) {
+  var resposta = prompt("ja visitou alguma outra cidade? ")
+  var nomeCidade = prompt("Qual o nome da cidade? ")
+}
+
