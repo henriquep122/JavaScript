6ce53e3f85e035759f8f400b8c504726cb73e1f9
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-07-28 23:55:43 -0300

    Exercício de For

---
+++ OniBitCode/exe06/tabuada.js
@@ -0,0 +1,8 @@
+let numero = prompt("Informe o número que você deseja calcular a tabuada: ")
+let resultado = ""
+
+for(let fator = 1; fator <= 20; fator++){
+  resultado += `${numero} x ${fator} = ${numero*fator}\n`
+}
+
+alert(`Resultado da tabuada de ${numero}:\n ${resultado}\n`)
