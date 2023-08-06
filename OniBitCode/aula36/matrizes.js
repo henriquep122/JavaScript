88f94da49afbb80ad28aa536b52a74f36dd8f144
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-08-06 17:41:15 -0300

    Update matrizes.js

--- OniBitCode/aula36/matrizes.js
+++ OniBitCode/aula36/matrizes.js
@@ -13,3 +13,19 @@ console.log(arr[1])
 console.log(arr[1][0])
 console.log(arr[1][1])
 console.log(arr[2][1][1])
+
+const matriz = [
+  ["l1, c1", "l1, c2", "l1, c3", "l1, c4"],
+  ["l2, c1", "l2, c2", "l2, c3", "l2, c4"],
+  ["l3, c1", "l3, c2", "l3, c3", "l3, c4"],
+]
+
+/*console.table(matriz)
+
+matriz.push(["Nova linha"])
+matriz[0].push("Nova coluna")
+console.table(matriz)*/
+
+for(let i = 0; i < matriz.length; i++){
+  console.log(matriz[i])
+}
