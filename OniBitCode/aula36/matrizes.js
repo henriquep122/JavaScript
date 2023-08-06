ee56a57ef23047062a5c8b9dea1af749bbe4145c
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-08-06 17:19:58 -0300

    Update matrizes.js

--- OniBitCode/aula36/matrizes.js
+++ OniBitCode/aula36/matrizes.js
@@ -1,10 +1,15 @@
 const arr = [
-  ["1° Nivel",
-  "2° Nivel",
-  "3° Nivel"],
-  ["1° Nivel",
-  "2° Nivel, 1° item", "Olá, mundo!",
-  "3° Nivel"],
+  "1° Nivel",
+  ["2° Nivel", 42, true],
+  [
+    ["3° Nivel, 1° item", "Olá, mundo!"],
+    ["3° Nivel, 2° item", "Oi, mundo!"],
+  ],
   []
 ]
 console.log(arr)
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[1][0])
+console.log(arr[1][1])
+console.log(arr[2][1][1])
