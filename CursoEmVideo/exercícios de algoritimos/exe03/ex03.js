682918ab8d2c710d27684c7596ff6ff7d2e1babc
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-08-13 00:14:17 -0300

    ex03

---
+++ CursoEmVideo/exercícios de algoritimos/exe03/ex03.js
@@ -0,0 +1,8 @@
+let nome = prompt("Qual seu nome? ")
+let salario = prompt("Qual é o seu salário? ")
+
+alert(
+`Nome do(a) funcionário(a): ${nome}
+Salário: ${salario}
+O(a) funcionário(a) ${nome} tem um salário de R$${salario} em Junho.`
+)
