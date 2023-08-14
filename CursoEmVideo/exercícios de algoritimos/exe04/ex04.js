ce004645e7c83bc12bb8b7b68858baeec77846fb
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-08-13 23:46:13 -0300

    exercício simples

---
+++ CursoEmVideo/exercícios de algoritimos/exe04/ex04.js
@@ -0,0 +1,6 @@
+let n1 = Number(prompt("Digite um valor: "))
+let n2 = Number(prompt("Digite outro valor: "))
+
+let soma = n1 + n2
+
+alert(`A soma entre ${n1} e ${n2} é igual a ${soma}`)
---
+++ CursoEmVideo/exercícios de algoritimos/exe05/ex05.js
@@ -0,0 +1,5 @@
+let nota1 = Number(prompt("Informe a nota! \n\nNota 1:"))
+let nota2 = Number(prompt("Nota 2: "))
+let media = (nota1 + nota2)/2
+
+alert(`A média entre ${nota1} e ${nota2} é igual a ${media}`)
