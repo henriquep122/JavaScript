c4f003e0e625cef3505e1611ac8cc66799bf62da
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-08-14 23:59:39 -0300

    Exercício 8

---
+++ CursoEmVideo/exercícios de algoritimos/exe08/ex08.js
@@ -0,0 +1,9 @@
+const metros = Number(prompt("Digite uma distância em metros: "))
+
+alert(`A distância de ${metros}m corresponde a: 
+${metros / 1000}Km
+${metros / 100}Hm
+${metros / 10}Dam
+${metros * 10}dm
+${metros * 100}cm
+${metros * 1000}mm`)
