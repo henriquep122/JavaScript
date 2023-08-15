2d5bebba3c077f83bb2ad7691b3b9eb0f7055e10
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-08-15 19:03:22 -0300

    Exercício 9

---
+++ CursoEmVideo/exercícios de algoritimos/exe09/ex09.js
@@ -0,0 +1,5 @@
+let salario = Number(prompt("Quanto de dinheiro você tem na carteira? "))
+
+let dolar = (salario / 3.45).toFixed(2)
+
+alert(`Você tem R$${salario} e com isso você pode comprar U$${dolar}`)
