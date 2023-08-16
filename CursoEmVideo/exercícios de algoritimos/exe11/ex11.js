6d7b31cf2f0462878c178ae67e0d63409ef7cd34
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-08-15 23:39:50 -0300

    Exercício 10

---
+++ CursoEmVideo/exercícios de algoritimos/exe11/ex11.js
@@ -0,0 +1,7 @@
+let a = Number(prompt("Informe o valor de A: "))
+let b = Number(prompt("Informe o valor de B: "))
+let c = Number(prompt("Informe o valor de C: "))
+
+let delta = (b**2 - (4 * a * c)).toFixed(2)
+
+alert(`O valor de delta é ${delta}`)
