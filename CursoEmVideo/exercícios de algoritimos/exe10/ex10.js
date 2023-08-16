e1aabe681bb00c3b331940e34dbb0c40c5dfd6ab
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-08-15 22:50:28 -0300

    Exercício 10

---
+++ CursoEmVideo/exercícios de algoritimos/exe10/ex10.js
@@ -0,0 +1,7 @@
+const altura = Number(prompt("Informe a altura da parede: "))
+const largura = Number(prompt("Informe a largura da parede: "))
+
+let metros = altura * largura
+
+alert(`A área a ser pintada é de ${metros}².
+A quantidade de tinta nescessária para pintar essa parede é de ${metros/2}`)
