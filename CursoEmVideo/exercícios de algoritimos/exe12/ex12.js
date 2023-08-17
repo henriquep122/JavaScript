245b780a61373fedb564d0caa490eb3e81611d95
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-08-17 19:40:09 -0300

    Update ex12.js

--- CursoEmVideo/exercícios de algoritimos/exe12/ex12.js
+++ CursoEmVideo/exercícios de algoritimos/exe12/ex12.js
@@ -2,4 +2,4 @@ let precoProduto = Number(prompt("Informe o preço do produto: R$"))
 const desconto = 0.05 * precoProduto
 let total = precoProduto - desconto 
 
-alert(`O Preço Promocional do produto é de: R$${total.toFixed(2)}`)
+alert(`O Preço Promocional do produto é de: R$${total.toFixed(2)} reias`)
