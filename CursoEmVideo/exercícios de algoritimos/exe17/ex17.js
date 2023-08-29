2ba0a65887061aaaa006417ee0c617b5e044723c
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-08-29 13:28:12 -0300

    Atualizando

--- CursoEmVideo/exercícios de algoritimos/exe17/ex17.js
+++ CursoEmVideo/exercícios de algoritimos/exe17/ex17.js
@@ -1,5 +1,6 @@
 let velocidade = Number(prompt("Qual era a velocidade do seu carro(km)? "))
 
 if (velocidade > 80) {
-  
+  alert(`Você foi multado por excesso de velocidade!
+O valor da sua multa é de R$${(velocidade-80)*5} reais`)
 }
