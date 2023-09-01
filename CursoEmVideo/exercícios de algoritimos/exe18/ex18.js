e6f5b6a30f09879e10eee7a78371c507791f7361
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-08-31 23:04:21 -0300

    Update ex18.js

--- CursoEmVideo/exercícios de algoritimos/exe18/ex18.js
+++ CursoEmVideo/exercícios de algoritimos/exe18/ex18.js
@@ -1,1 +1,11 @@
 let anoNascimento = Number(prompt("Informe seu ano de nascimento: "))
+const dataAtual = new Date()
+const anoAtual = dataAtual.getFullYear()
+
+let idade = anoAtual - anoNascimento
+
+if (idade >= 18) {
+  alert("Você tem idade suficiente para votar!")
+} else {
+  alert("Você não tem idade para votar!")
+}
