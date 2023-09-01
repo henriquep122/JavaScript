081dd2de3ba37bc96bb086267dfbff3a3b08471f
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-08-31 23:11:45 -0300

    Update ex18.js

--- CursoEmVideo/exercícios de algoritimos/exe18/ex18.js
+++ CursoEmVideo/exercícios de algoritimos/exe18/ex18.js
@@ -4,8 +4,12 @@ const anoAtual = dataAtual.getFullYear()
 
 let idade = anoAtual - anoNascimento
 
-if (idade >= 18) {
-  alert("Você tem idade suficiente para votar!")
+if (idade >= 16 && idade >= 60) {
+  alert("Você tem idade para votar! (Voto não obrigatório)")
+} if (idade >= 18) {
+  alert("Você tem idade suficiente para votar! (Voto obrigatório)")
 } else {
+  
+} {
   alert("Você não tem idade para votar!")
 }
