ad7281a73f5749c4a7616d5a1acddd3d8e451a2f
Henrique Pereira Da Silva <henriquepereira122@gmail.com>
2023-09-05 22:40:41 -0300

    Update ex19.js

--- CursoEmVideo/exercícios de algoritimos/exe19/ex19.js
+++ CursoEmVideo/exercícios de algoritimos/exe19/ex19.js
@@ -1,1 +1,14 @@
 
+let nome = prompt("Informe o nome do aluno: ")
+let n1 = Number(prompt("Informe a 1° nota do aluno: "))
+let n2 = Number(prompt("Informe a 2° nota do aluno: "))
+
+let nota = ((n1+n2)/2).toFixed(2)
+
+alert(`A nota do aluno é de ${nota}pts`)
+
+if (nota >= 7) {
+  alert(`O aluno ${nome} está na média com a nota ${nota} - [Aprovado!]`)
+} else {
+  alert(`O aluno ${nome} não está na média! nota ${nota} - [Reprovado!]`)
+}
